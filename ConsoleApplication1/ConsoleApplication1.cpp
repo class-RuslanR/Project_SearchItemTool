cb00df15f3af6e62b17b38a19b3160f5f427cb49
Ruslan Rogozhkin <57987419+class-RuslanR@users.noreply.github.com>
2020-01-21 00:01:35 +0200

    Добавил методы set, get и show для полей характеристик режущего инструмента базового класса. Создал пустые производные классы

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -4,16 +4,30 @@
 #include "DataBaseTool.h"
 
 using namespace std;
-//using namespace BaseTool;
 
 int main()
 {
 	setlocale(LC_ALL, "ru");
 
 	BaseTool::Tool diam;
-	diam.set_diam(10);
+	diam.set_diam(10.5);
 	diam.get_diam();
 	diam.show_diam();
+
+	BaseTool::Tool length;
+	length.set_lengthCut(5.8);
+	length.get_lengthCut();
+	length.show_lengthCut();
+
+	BaseTool::Tool Vc;
+	Vc.set_V_c(40.5);
+	Vc.get_V_c();
+	Vc.show_V_c();
+
+	BaseTool::Tool Fz;
+	Fz.set_f_z(0.25);
+	Fz.get_f_z();
+	Fz.show_f_z();
 
 	system("pause");
 	return 0;
--- ConsoleApplication1/DataBaseTool.h
+++ ConsoleApplication1/DataBaseTool.h
@@ -6,13 +6,13 @@ using namespace std;
 
 namespace BaseTool
 {
-	class Tool // базовый класс режущего инструмента
+	class Tool // base class cutting tool
 	{
 	protected:
-		double diam; // диаметр инструмента
-		double lengthCut; // длина режущей части
-		double V_c; // скорость резания
-		double f_z; //подача на зуб
+		double diam; // diametre of tool
+		double lengthCut; // length of cutting part
+		double V_c; // cutting speed
+		double f_z; //tooth feed
 	public:
 		Tool()
 		{
@@ -21,7 +21,44 @@ namespace BaseTool
 		void set_diam(double diam); 
 		double get_diam();
 		void show_diam();
+
+		void set_lengthCut(double lengthCut);
+		double get_lengthCut();
+		void show_lengthCut();
+
+		void set_V_c(double V_c);
+		double get_V_c();
+		void show_V_c();
+
+		void set_f_z(double diam);
+		double get_f_z();
+		void show_f_z();
 	};
+
+	class MillingCutter : public Tool
+	{
+	protected:
+		string Brand;
+	public:
+
+	};
+
+	class FaceMillCutter : public MillingCutter
+	{
+
+	};
+
+	class EndMillCutter : public MillingCutter
+	{
+
+	};
+
+	class T_shapedMillCutter : public MillingCutter
+	{
+
+	};
+
+	//Methods of class Tool:
 
 	void BaseTool::Tool::set_diam(double diam)
 	{
@@ -38,4 +75,57 @@ namespace BaseTool
 		cout << "Диаметр инструмента = " << this->diam << "\n" << endl;
 	}
 
+	////////////////////////////////////////////////////////////
+
+
+	void BaseTool::Tool::set_lengthCut(double lengthCut)
+	{
+		this->lengthCut = lengthCut;
+	}
+
+	double BaseTool::Tool::get_lengthCut()
+	{
+		return this->lengthCut;
+	}
+
+	inline void BaseTool::Tool::show_lengthCut()
+	{
+		cout << "Длина режущей части = " << this->lengthCut << "\n" << endl;
+	}
+
+	/////////////////////////////////////////////////////////////
+
+	void BaseTool::Tool::set_V_c(double V_c)
+	{
+		this->V_c = V_c;
+	}
+
+	double BaseTool::Tool::get_V_c()
+	{
+		return this->V_c;
+	}
+
+	inline void BaseTool::Tool::show_V_c()
+	{
+		cout << "Скорость резания по каталогу = " << this->V_c << "\n" << endl;
+	}
+
+	////////////////////////////////////////////////////////////
+
+	void BaseTool::Tool::set_f_z(double f_z)
+	{
+		this->f_z = f_z;
+	}
+
+	double BaseTool::Tool::get_f_z()
+	{
+		return this->f_z;
+	}
+
+	inline void BaseTool::Tool::show_f_z()
+	{
+		cout << "Подача на зуб по каталогу = " << this->f_z << "\n" << endl;
+	}
+
+	////////////////////////////////////////////////////////////////
 }
